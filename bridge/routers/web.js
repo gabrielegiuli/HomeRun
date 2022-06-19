ac1cbc59f17bba9ebfc2a37e6efc987f7b5cf2ac
gabrielegiuli <gabriele.giuli18@imperial.ac.uk>
2022-06-19 22:18:30 +0200

    basis of dashboard added

--- bridge/routers/web.js
+++ bridge/routers/web.js
@@ -4,7 +4,7 @@ const path = require('path')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../web/index.html'))
 })
 
 module.exports = router
